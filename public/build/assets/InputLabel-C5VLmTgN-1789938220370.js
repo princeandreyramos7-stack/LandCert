e64f109d63fceca25d690a5b5def2c422d8a0398
Prinsipe <princeandreyramos7@gmail.com>
2026-09-21 11:49:23 +0800

    Updates

--- public/build/assets/InputLabel-C5VLmTgN-1789938220370.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Ta5O09Vn-1789938220370.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
--- public/build/assets/chevron-up-DUnt005g-1789938220370.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./app-Ta5O09Vn-1789938220370.js";const c=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("chevron-up",c);export{n as C};
